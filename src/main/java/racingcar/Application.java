b1160370e0a0b56d0c11b555fb45afdf69ab9cb0
seongsoo-jeon <stome159@naver.com>
2024-02-19 22:16:07 +0900

    자동차 이름, 시도 횟수 입력 기능 구현 Random, Console

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,13 +3,14 @@ package racingcar;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         // TODO 구현 진행
         Scanner scan = new Scanner(System.in);
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
-        String[] nameInput = scan.nextLine().split(",");
+        String[] nameInput = Console.readLine().split(",");
 
         List<Car> carList = new ArrayList<Car>();
         for(int i = 0;i < nameInput.length;i++) {
@@ -17,7 +18,7 @@ public class Application {
         }
 
         System.out.println("시도할 횟수는 몇회인가요?\n");
-        int count = scan.nextInt();
+        int count = Integer.valueOf(Console.readLine());
 
 
     }
