c6a68b93b9660f3ef07298918e49d4517a6f9168
seongsoo-jeon <stome159@naver.com>
2024-02-19 22:41:56 +0900

    각 실행에 필요한 기능 구현 랜덤숫자로 이동 여부 판단 및 각 인스턴스에 대한 상태 출력까지

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -4,9 +4,22 @@ public class Car {
     private final String name;
     private int position = 0;
 
+    private static int playerNum = 0;
     public Car(String name) {
         this.name = name;
+        playerNum++;
     }
 
+    public void move() {
+        this.position++;
+    }
+
+    public void printPos() {
+        System.out.print(this.name + " : ");
+        for(int i = 0;i < this.position;i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
     // 추가 기능 구현
 }
--- src/main/java/racingcar/Control.java
+++ src/main/java/racingcar/Control.java
@@ -1,14 +1,65 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Control {
     int countGame;
     List<Car> player;
+    int playerNum;
 
-    public Control(List<Car> player, int countGame) {
-        this.player = player;
-        this.countGame = countGame;
+    public Control() {
+        gameSet();
     }
+
+
+    public void gameSet() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String[] nameInput = Console.readLine().split(",");
+
+        List<Car> carList = new ArrayList<Car>();
+        for(int i = 0;i < nameInput.length;i++) {
+            carList.add(new Car(nameInput[i]));
+        }
+
+        System.out.println("시도할 횟수는 몇회인가요?");
+        int count = Integer.valueOf(Console.readLine());
+
+        this.player = player;
+        this.countGame = count;
+        this.playerNum = nameInput.length;
+
+        for(int i = 1;i <= this.countGame;i++) {
+            gameStart(i);
+        }
+
+
+    }
+    public void gameStart(int n) {
+        System.out.println("실행 결과");
+        for(int i = 0;i < playerNum;i++) {
+            if(Randoms.pickNumberInRange(0, 9) >= 4) {
+                this.player.get(0).move();
+                this.player.get(0).printPos();
+            }
+        }
+        System.out.println();
+
+        if(n == countGame) {
+            printWinner();
+        }
+
+    }
+
+    public void printResult() {
+
+    }
+
+    public void printWinner() {
+
+    }
+
 }
